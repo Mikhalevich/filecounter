6ba144177765a4b98111fb34a5a2244ecee64ab7
Michael Mikhalevich <mike@viber.com>
2017-11-16 12:05:35 +0300

    Computing results logic was rewrited

--- main.go
+++ main.go
@@ -88,10 +88,27 @@ func (self FileInfo) String() string {
 	return fmt.Sprintf("Path = %s; Size = %s; LineCount = %d", self.Path, self.Size, self.Lines)
 }
 
-type TotalFileInfo struct {
+type GroupFileInfo struct {
 	Count int
 	Size  ByteSize
 	Lines int
+}
+
+func (self GroupFileInfo) String() string {
+	return fmt.Sprintf("Count = %d; Sizse %s; LineCount = %d", self.Count, self.Size, self.Lines)
+}
+
+type PrintFileInfo struct {
+	Extentions map[string]GroupFileInfo
+	Total      GroupFileInfo
+	Print      []FileInfo
+}
+
+func NewPrintFileInfo() *PrintFileInfo {
+	return &PrintFileInfo{
+		Extentions: make(map[string]GroupFileInfo),
+		Print:      make([]FileInfo, 0),
+	}
 }
 
 func parseArguments() (*Params, error) {
@@ -215,37 +232,47 @@ func walkFiles(params *Params) ([]FileInfo, []error) {
 	return results, errs
 }
 
-func printResults(params *Params, results []FileInfo, errors []error) {
-	totalFiles := make(map[string]TotalFileInfo)
-	totalFilesCount := 0
-	totalLinesCount := 0
+func computeResults(params *Params, results []FileInfo) *PrintFileInfo {
+	pfi := NewPrintFileInfo()
 
 	sort.Slice(results, func(i, j int) bool { return results[i].Lines < results[j].Lines })
 
 	for _, info := range results {
 		if params.PrintLines >= 0 && params.PrintLines < info.Lines {
-			fmt.Println(info)
+			pfi.Print = append(pfi.Print, info)
 		}
-		totalInfo := totalFiles[info.Extention]
-		totalInfo.Count++
-		totalInfo.Size += info.Size
-		totalInfo.Lines += info.Lines
-		totalFiles[info.Extention] = totalInfo
+		extInfo := pfi.Extentions[info.Extention]
+		extInfo.Count++
+		extInfo.Size += info.Size
+		extInfo.Lines += info.Lines
+		pfi.Extentions[info.Extention] = extInfo
 
-		totalFilesCount += 1
-		totalLinesCount += info.Lines
+		pfi.Total.Count += 1
+		pfi.Total.Lines += info.Lines
+		pfi.Total.Size += info.Size
 	}
 
-	fmt.Println("File count by suffix:")
-	for ext, info := range totalFiles {
-		fmt.Printf("%s => count = %d; size = %s; lines = %d\n", ext, info.Count, info.Size, info.Lines)
-	}
-	fmt.Printf("Total files = %d\n", totalFilesCount)
-	fmt.Printf("Total lines = %d\n", totalLinesCount)
+	return pfi
+}
 
+func printResults(pfi *PrintFileInfo, errors []error) {
+	fmt.Println("Files:")
+	for _, info := range pfi.Print {
+		fmt.Println(info)
+	}
+
+	fmt.Println("Errors:")
 	for _, err := range errors {
 		fmt.Printf("Error: %v\n", err)
 	}
+
+	fmt.Println("File count by suffix:")
+	for ext, info := range pfi.Extentions {
+		fmt.Printf("%s => count = %d; size = %s; lines = %d\n", ext, info.Count, info.Size, info.Lines)
+	}
+
+	fmt.Println("Total file info:")
+	fmt.Println(pfi.Total)
 }
 
 func main() {
@@ -258,7 +285,8 @@ func main() {
 	}
 
 	results, errors := walkFiles(params)
-	printResults(params, results, errors)
+	cfr := computeResults(params, results)
+	printResults(cfr, errors)
 
 	fmt.Printf("Execution time = %v\n", time.Now().Sub(startTime))
 }
